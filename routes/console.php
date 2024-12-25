f7f1d3b85987ed2f26ea3f1ff916185b6dc0d8d9
Vipurthanan <lessandsass@gmail.com>
2024-12-25 16:44:38 +0530

    adding parameters
    - We can chain on other parameters too.

--- routes/console.php
+++ routes/console.php
@@ -4,4 +4,8 @@ use App\Console\Commands\OverdueTodos;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('todos:overdue');
+Artisan::command('todos:overdue')
+    ->daily()
+    ->at('06:00')
+    ->timezone('America/New_York')
+    ->weekdays();
