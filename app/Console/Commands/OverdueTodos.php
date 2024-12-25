6a17b8aff5eae2a2a72e5ba094ff03e951c4fc5d
Vipurthanan <lessandsass@gmail.com>
2024-12-25 16:17:23 +0530

    creating command to send queue mails
    - Making command to send email
    - The command name was added as ‘OverdueTodos’

---
+++ app/Console/Commands/OverdueTodos.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class OverdueTodos extends Command
+{
+    protected $signature = 'app:overdue-todos';
+
+    protected $description = 'Command description';
+
+    public function handle()
+    {
+        //
+    }
+}
