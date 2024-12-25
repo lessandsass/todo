54ffee48c6739f3239f70fde9d1f0bac4f88dd6b
Vipurthanan <lessandsass@gmail.com>
2024-12-25 16:35:58 +0530

    passing the signature.
    - Executing the command by passing the declared signature.

--- routes/console.php
+++ routes/console.php
@@ -4,4 +4,4 @@ use App\Console\Commands\OverdueTodos;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command(OverdueTodos::class);
+Artisan::command('todos:overdue');
